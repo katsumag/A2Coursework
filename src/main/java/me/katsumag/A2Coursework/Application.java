8f22ad31871167681c6d1ef09999db167d834be8
James <goude47@gmail.com>
2022-06-13 21:31:49 +0100

    Use borderpane to organise layout into different panes, use vbox for vertical stacking of the left pane, and keep placeholder text for remaining space - the "center" pane (consumes the rest of the available space)

--- src/main/java/me/katsumag/A2Coursework/Application.java
+++ src/main/java/me/katsumag/A2Coursework/Application.java
@@ -2,15 +2,29 @@ package me.katsumag.A2Coursework;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        // construct GUI
+        BorderPane borderPane = new BorderPane();
+
+        // construct left panel
+        VBox leftPane = new VBox();
+        Label leftTitle = new Label("Logic Components");
+        leftPane.getChildren().add(leftTitle);
+        borderPane.setLeft(leftPane);
+
+        // construct center panel
         Label label = new Label("Hello, world!");
-        Scene scene = new Scene(new StackPane(label), 640, 480);
+        borderPane.setCenter(label);
+
+        Scene scene = new Scene(borderPane, 640, 480);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
