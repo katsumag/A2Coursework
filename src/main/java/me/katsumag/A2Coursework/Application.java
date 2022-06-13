3e7c318106b6b21da27e59473d210b4a707a5844
James <goude47@gmail.com>
2022-06-13 22:03:46 +0100

    Tried to add an SVG as an image to a JavaFX scene, but JavaFX has no support for it. No error is thrown, image fails to load. jpg images work fine though.

--- src/main/java/me/katsumag/A2Coursework/Application.java
+++ src/main/java/me/katsumag/A2Coursework/Application.java
@@ -2,6 +2,7 @@ package me.katsumag.A2Coursework;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -17,7 +18,14 @@ public class Application extends javafx.application.Application {
         // construct left panel
         VBox leftPane = new VBox();
         Label leftTitle = new Label("Logic Components");
-        leftPane.getChildren().add(leftTitle);
+
+        ImageView imageView = new ImageView(new SVGLoader().load("./images/AND gate.svg"));
+        imageView.setX(10);
+        imageView.setY(10);
+        imageView.setFitWidth(400);
+        imageView.setPreserveRatio(true);
+
+        leftPane.getChildren().addAll(leftTitle, imageView);
         borderPane.setLeft(leftPane);
 
         // construct center panel
