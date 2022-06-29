c5aa806e8cb9e94bead3558bc55a0e3452f864dc
James <goude47@gmail.com>
2022-06-29 13:02:33 +0100

    Minor comment improvements

--- src/main/java/me/katsumag/A2Coursework/EventHandlers/GateStopDragHandler.java
+++ src/main/java/me/katsumag/A2Coursework/EventHandlers/GateStopDragHandler.java
@@ -20,16 +20,16 @@ public class GateStopDragHandler implements EventHandler<DragEvent> {
 
         // Handle drag and drop movements inside the center pane to relocate a component
         // Establish that the component wasn't dragged from the VBox of the left BorderPane
+        // No check is required for the gesture target, as this event handler can only be called by the center pane
         if (!(image.getParent() instanceof VBox)) {
             // Establish that the component is assigned to the center Pane
-            // must be checked in this order as VBox extends Pane
+            // Must be checked in this order as VBox extends Pane
             if (image.getParent() instanceof Pane) {
                 image.relocate(event.getX(), event.getY());
+                // Stop execution here so that a new component isn't added.
                 return;
             }
         }
-        
-        // No check is required for the gesture target, as this event handler can only be called by the center pane
 
         // The get the type of image to create
         CircuitComponentType type = CircuitComponentType.valueOf((String) image.getProperties().get("CircuitComponentType"));
@@ -44,7 +44,7 @@ public class GateStopDragHandler implements EventHandler<DragEvent> {
 
         newComponent.getImage().relocate(event.getX(), event.getY());
 
-        // I know that the target will be a TilePane (for now, anyway) as that's the type our center panel is
+        // I know that the target will be a Pane as that's the type our center panel is
         ((Pane) event.getGestureTarget()).getChildren().add(newComponent.getImage());
 
         event.setDropCompleted(true);
