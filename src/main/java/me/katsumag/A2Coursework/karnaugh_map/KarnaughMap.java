b1b3f6aed5f584b7f3ac49eeeacdaa9c1d501f72
James <goude47@gmail.com>
2022-10-12 16:18:54 +0100

    Attempt to write code to sort the map in correct gray code order for x and y. Fails because you can't add() at an index more than 1 over what the current size is, so I'll need to prefill each row and then use set.

--- src/main/java/me/katsumag/A2Coursework/karnaugh_map/KarnaughMap.java
+++ src/main/java/me/katsumag/A2Coursework/karnaugh_map/KarnaughMap.java
@@ -3,7 +3,9 @@ package me.katsumag.A2Coursework.karnaugh_map;
 import javafx.scene.input.MouseEvent;
 import me.katsumag.A2Coursework.truth_table.TruthTable;
 
+import javax.imageio.plugins.tiff.ExifInteroperabilityTagSet;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class KarnaughMap {
 
@@ -12,22 +14,80 @@ public class KarnaughMap {
 
         if (truthTable == null) { return; }
 
+        System.out.println("passed null check");
+
         List<List<Boolean>> emptyMap = getEmptyMap((int) Math.sqrt(truthTable.size()));
 
-        truthTable.forEach((inputs, output) -> {
-            List<Boolean> binaryY = inputs.subList(0, inputs.size() / 2);
-            List<Boolean> binaryX = inputs.subList(inputs.size() / 2, inputs.size());
+        for (Map.Entry<List<Boolean>, Boolean> entrySet : truthTable.entrySet()) {
+
+            // because the map is already filled with 0s, only need to set 1s
+            if (! entrySet.getValue()) { continue; }
+
+            List<Boolean> binaryY = entrySet.getKey().subList(0, entrySet.getKey().size() / 2);
+            List<Boolean> binaryX = entrySet.getKey().subList(entrySet.getKey().size() / 2, entrySet.getKey().size());
 
             int y = BinaryToDenary.convert(binaryY);
             int x = BinaryToDenary.convert(binaryX);
 
-            emptyMap.get(y).set(x, output);
-        });
+            emptyMap.get(y).set(x, entrySet.getValue());
+        }
+
+        System.out.println("Added entries to map");
 
         emptyMap.forEach(System.out::println);
 
         //TODO: gray code order, expression generation
 
+        System.out.println("doing new stuff");
+        int grayCodeIteration = (int) (Math.sqrt(getSizeOfMap(emptyMap)) / 2);
+        List<List<Boolean>> rawGrayCode = getGrayCode(grayCodeIteration);
+        List<Integer> grayCode = convertGrayCodeToInts(rawGrayCode);
+        emptyMap = sortXByGrayCode(emptyMap, grayCode);
+        System.out.println("X sorted");
+        emptyMap.forEach(System.out::println);
+        emptyMap = sortYByGrayCode(emptyMap, grayCode);
+        System.out.println("Sorted maps");
+        emptyMap.forEach(System.out::println);
+        System.out.println("Finished");
+        sortYByGradeCode(emptyMap, grayCode);
+        //return emptyMap;
+    }
+
+    private int getSizeOfMap(List<List<Boolean>> map) {
+        return map.size() * map.get(0).size();
+    }
+
+    private List<List<Boolean>> sortYByGrayCode(List<List<Boolean>> map, List<Integer> grayCode) {
+        List<List<Boolean>> newSortedMap = new ArrayList<>();
+        for (int i = 0; i < grayCode.size(); i++) {
+            newSortedMap.add(new ArrayList<>());
+        }
+
+        grayCode.forEach(code -> map.forEach(row -> newSortedMap.get(code).add(row.get(code))));
+
+        return newSortedMap;
+    }
+
+    private void sortYByGradeCode(List<List<Boolean>> map, List<Integer> grayCode) {
+        List<List<Boolean>> newSortedMap = new ArrayList<>();
+        map.forEach(row -> {
+            List<Boolean> newRow = new ArrayList<>();
+            for (int i = 0; i < grayCode.size(); i++) {
+                newRow.add(grayCode.get(i), row.get(i));
+            }
+            newSortedMap.add(newRow);
+        });
+        newSortedMap.forEach(System.out::println);
+    }
+
+    private List<List<Boolean>> sortXByGrayCode(List<List<Boolean>> map, List<Integer> grayCode) {
+        List<List<Boolean>> newSortedMap = new ArrayList<>();
+        grayCode.forEach(index -> newSortedMap.add(map.get(index)));
+        return newSortedMap;
+    }
+
+    private List<Integer> convertGrayCodeToInts(List<List<Boolean>> grayCode) {
+        return grayCode.stream().map(BinaryToDenary::convert).collect(Collectors.toList());
     }
 
     private List<List<Boolean>> getEmptyMap(int inputCount) {
@@ -45,6 +105,13 @@ public class KarnaughMap {
         return emptyMap;
     }
 
+    /*
+    Get next gray code sequence from previous by:
+    - reversing a copy of the previous codes
+    - adding 1 in front of each of them
+    - adding 0 in front of each of the previous codes
+    - collecting the reversed and previous codes into one list
+     */
     private List<List<Boolean>> generateNextGrayCode(List<List<Boolean>> previous) {
         // reverse previous - need to copy it
         List<List<Boolean>> reflected = new ArrayList<>();
@@ -59,7 +126,10 @@ public class KarnaughMap {
         return previous;
     }
 
-    private List<List<Boolean>> getGrayCode(int nIteration) {
+    /*
+    creates gray codes up to (and including) iteration n, see https://en.wikipedia.org/wiki/Gray_code
+     */
+    private List<List<Boolean>> getGrayCode(final int nIteration) {
         // create base case n=1, [0, 1]
         List<List<Boolean>> grayCode = new ArrayList<>(Arrays.asList(toMutableList(List.of(false)), toMutableList(List.of(true))));
         if (nIteration <= 1) { return  grayCode; }
